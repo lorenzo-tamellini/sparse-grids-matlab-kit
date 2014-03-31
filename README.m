14ad7d06cf8c01e5862a54ac1686cf16de8d293c
tamellin <tamellin@epfl>
2014-03-31 15:45:20 +0000

    added copyright to readme and release notes
    git-svn-id: https://svn.epfl.ch/svn/csqi-sparse-grids-matlab-kit@34 f6d9bf71-34f0-4f25-84af-45f13ff2d224

--- README.m
+++ README.m
@@ -1,5 +1,7 @@
 %----------------------------------------------------------------------------------
-% CSQI - Sparse Grids Matlab Kit
+% Sparse Grids Matlab Kit
+% Copyright (c) 2009-2014 L. Tamellini, F. Nobile
+% See LICENSE.txt for license
 %----------------------------------------------------------------------------------
 
 
