f3af9b58f7d6fc68c62964db38588428bd3d4ad9
ChiaraPiazzola <chiara.piazzola@imati.cnr.it>
2022-02-21 12:23:06 +0100

    correct recurrence relations for orthonormal polynomials

--- tools/polynomials_functions/jacobi_eval.m
+++ tools/polynomials_functions/jacobi_eval.m
@@ -15,21 +15,17 @@ function L = jacobi_eval(x,k,alpha,beta,a,b)
 % See LICENSE.txt for license
 %-------------------------------------------------------------
 
-
-
 % this function expresses L as a function of the standard Jacobi "probabilistic" polynomial (i.e. orthoGONAL w.r.t. 
 % rho(x)=Gamma(alpha+beta+2)/(Gamma(alpha+1)*Gamma(beta+1)*2^(alpha+beta+1))*(1+x)^alpha*(1-x)^beta,
 % which are recursively calculated through the function standard_jac_eval, coded below in this .m file
 
-% first compute the transformation of x (referred to Beta(alpha,beta,x_a,x_b) ) to z, the standard Beta(alpha,Beta,-1,1)
-
-z = ( 2*x - a - b ) / ( a - b ) ;
+z = ( -2*x + a + b) / (b - a) ;
 
 % calculate the standard Legendre polynomials in t
 L = standard_jac_eval(z,k,alpha,beta);
 % modify L to take into account normalizations. 
-if k>1
-    C = gamma(k+alpha)*gamma(k+beta)/(gamma(alpha+1)*gamma(beta+1))*gamma(alpha+beta+2)/((2*(k-1)+alpha+beta+1)*(k+alpha+beta)*factorial(k-1));
+if k>=1
+    C = gamma(alpha+beta+2)*gamma(k+alpha+1)*gamma(k+beta+1)/((2*k+alpha+beta+1)*gamma(k+alpha+beta+1)*factorial(k)*gamma(alpha+1)*gamma(beta+1));
     L = L / sqrt ( C );
 end
 
@@ -47,7 +43,7 @@ function L = standard_jac_eval(x,k,alpha,beta)
 % rho(x)=Gamma(alpha+beta+2)/(Gamma(alpha+1)*Gamma(beta+1)*2^(alpha+beta+1))*(1+x)^alpha*(1-x)^beta
 % in the points x (x can be a vector as well)
 %
-% N.B. the polynomials start from k=0: L_0(x) = 1, L_1(x) = x-(beta-alpha)/(alpha+beta+2)
+% N.B. the polynomials start from k=0: L_0(x) = 1, L_1(x) = 0.5*(alpha-beta) + 0.5*(alpha+beta+2)*x
 
 % base steps
 
@@ -55,7 +51,7 @@ function L = standard_jac_eval(x,k,alpha,beta)
 L_2=ones(size(x));
 
 % and this as L(k-1)
-L_1=x-(beta-alpha)/(alpha+beta+2);
+L_1=0.5*(alpha-beta) + 0.5*(alpha+beta+2)*x;
 
 if k==0
       L=L_2;
@@ -66,9 +62,12 @@ elseif k==1
 else
       % recursive step
       for ric=2:k
-            c1 = (beta^2-alpha^2)./((2*(ric-1)+alpha+beta).*(2*(ric-1)+alpha+beta+2));
-            c2 = 4*(ric-1).*(ric-1+alpha).*(ric-1+beta).*(ric-1+alpha+beta)./((2*(ric-1)+alpha+beta).^2.*(2*(ric-1)+alpha+beta+1).*(2*(ric-1)+alpha+beta-1));
-            L = (x-c1).* L_1 - c2*L_2;
+            c1 = 2*ric.*(2*(ric-1)+alpha+beta).*(ric-1+alpha+beta+1); 
+            c2 = 2*(ric-1)+alpha+beta+1;
+            c3 = (2*(ric-1)+alpha+beta+2)*(2*(ric-1)+alpha+beta);
+            c4 = alpha^2-beta^2; 
+            c5 = 2*(ric-1+alpha)*(ric-1+beta).*(2*(ric-1)+alpha+beta+2); 
+            L = c2*(c3*x + c4)/c1.* L_1 - c5/c1*L_2;
             L_2=L_1;
             L_1=L;
       end
